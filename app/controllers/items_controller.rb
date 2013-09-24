24c58b94be5691d116e42abd5fd955b7c65520a8
Bucky Townsend <desnudopenguino@gmail.com>
2013-09-24 15:05:25 -0400

    cleaned up old commented out code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,6 @@
 class ItemsController < ApplicationController
-  #before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :update, :destroy]
   add_crumb "Lists", :lists_path
-  # GET /items
-  # GET /items.json
-  #def index
-  #  @list = List.find(params[:list_id])
-  #  @items = @list.items
-  #  @newItem = Item.new
-  #  add_crumb @list.name, list_path(@list) 
-  #  add_crumb "Items", list_items_path(@list)
-  #end
 
   # GET /items/1
   # GET /items/1.json
@@ -18,15 +8,6 @@ class ItemsController < ApplicationController
     add_crumb @item.list.name, list_path(@item.list) 
     add_crumb @item.name, item_path(@item)
   end
-
-  # GET /items/new
-  #def new
-  #  @item = Item.new
-  #end
-
-  # GET /items/1/edit
-  #def edit
-  #end
 
   # POST /items
   # POST /items.json
@@ -75,6 +56,10 @@ class ItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
+    end
+
+    def set_list
+      @list = Item.find(params[:id]).list
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
