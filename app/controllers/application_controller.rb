ef42a721d9c98dcce5c4e596c48fa8a033c2911f
Bucky Townsend <desnudopenguino@gmail.com>
2013-09-25 17:04:50 -0400

    added folder for fonts in assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # add root crumb
-  add_crumb "Home", '/'
-
   # rescue from 404 errors
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActionController::RoutingError, with: :not_found
--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-# Add fonts folder to assets path
-config.assets.paths << "#{Rails.root}/app/assets/fonts"
-
 module Lister
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +19,8 @@ module Lister
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # add fonts to asset pipeline
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
