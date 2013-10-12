bfaf75ffa600dc3fc2424846ffe06328c78b21c0
Bucky Townsend <desnudopenguino@gmail.com>
2013-10-11 22:45:36 -0400

    added objects to the show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show ]
 
   # GET users/:email
   def show
-
+    @lists = @user.lists
+    @newList = List.new
   end
 
   private 
     # Use callbacks to share common step or constrants between actions.
     def set_user
-      @user = User.find(params[:email])
+      @user = User.find(params[:id])
     end 
 end
